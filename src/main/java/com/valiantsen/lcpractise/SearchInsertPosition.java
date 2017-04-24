6c6054e267ee4193cd4a7b14938398df583ac0e4
Zhang Yasen <zhangyasen@didichuxing.com>
2017-04-24 22:24:12 +0800

    easy array tag

---
+++ src/main/java/com/valiantsen/lcpractise/SearchInsertPosition.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package main.java.com.valiantsen.lcpractise;
+
+/**
+ * @author zhangyasen
+ *
+ */
+public class SearchInsertPosition {
+	public int searchInsert(int[] nums, int target) {
+	       int low=0,high = nums.length-1;
+			 while(low<=high){
+			 	int mid = (low+high)/2;
+			 	if(nums[mid]==target){
+			 	return mid;
+			 	}else if(nums[mid]>target){
+			 		high=mid-1;
+			 	}else {
+			 		low=mid+1;
+			 	}
+			 }
+			 return low;
+	    }
+	
+	public static void main(String[] args) {
+		int m=1;
+		int n=2;
+		System.out.println(m=n==2?m:0);
+		System.out.println(m);
+	}
+}
