0b8f2266e67962067c4598767172c64312a9b622
Zhang Yasen <zhangyasen@didichuxing.com>
2017-04-28 10:13:40 +0800

    array tag

---
+++ src/main/java/com/valiantsen/lcpractise/ContainsDuplicateII.java
@@ -0,0 +1,29 @@
+package main.java.com.valiantsen.lcpractise;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array
+ * such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+ * 
+ * sliding window!!!
+ * 
+ * @ClassName: ContainsDuplicateII
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author zhangyasen@didichuxing.com
+ * @date 2017年4月27日
+ */
+public class ContainsDuplicateII {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k)
+                set.remove(nums[i - k - 1]);
+            if (!set.add(nums[i]))
+                return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/valiantsen/lcpractise/MissingNumber.java
@@ -0,0 +1,36 @@
+package main.java.com.valiantsen.lcpractise;
+
+/**
+ * Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+ * 
+ * For example,
+ * Given nums = [0, 1, 3] return 2.
+ * 
+ * Note:
+ * Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space
+ * complexity?
+ * 
+ * @ClassName: MissingNumber
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author zhangyasen@didichuxing.com
+ * @date 2017年4月27日
+ */
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int sum = 0, total = 0;
+        for (int i = 0; i < nums.length; i++) {
+            total += i;
+            sum += nums[i];
+        }
+        total += nums.length;
+        return total - sum;
+    }
+
+    public int missingNumberXOR(int[] nums) {
+        int xor = 0, i = 0;
+        for (; i < nums.length; i++) {
+            xor = xor ^ i ^ nums[i];
+        }
+        return xor ^ i;
+    }
+}
