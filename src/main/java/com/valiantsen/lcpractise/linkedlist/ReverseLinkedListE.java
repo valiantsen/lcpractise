4c8fdc844c6fc3bbcfd45c7f47c5058790286818
Zhang Yasen <zhangyasen@didichuxing.com>
2017-05-21 22:43:49 +0800

    reverse list is o(n)?

---
+++ src/main/java/com/valiantsen/lcpractise/linkedlist/ReverseLinkedListE.java
@@ -0,0 +1,29 @@
+package main.java.com.valiantsen.lcpractise.linkedlist;
+
+import main.java.com.valiantsen.lcpractise.linkedlist.RemoveDuplicatesFromSortedListE.ListNode;
+
+/**
+ * Reverse a singly linked list.
+ * 
+ * 
+ * @ClassName: ReverseLinkedListE
+ * @Description:
+ * @author zhangyasen
+ * @date 2017年5月21日
+ */
+public class ReverseLinkedListE {
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null)
+            return null;
+        ListNode first = null, second = head, third = second.next;
+        while (second != null) {
+            second.next = first;
+            first = second;
+            second = third;
+            if (third != null)
+                third = third.next;
+        }
+        return first;
+    }
+}
