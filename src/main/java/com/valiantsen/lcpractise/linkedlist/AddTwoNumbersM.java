98be821ad8714b5551ca35545ab3990a33d46a3f
Zhang Yasen <zhangyasen@didichuxing.com>
2017-05-29 23:25:59 +0800

    two nums

---
+++ src/main/java/com/valiantsen/lcpractise/linkedlist/AddTwoNumbersM.java
@@ -0,0 +1,56 @@
+package main.java.com.valiantsen.lcpractise.linkedlist;
+
+public class AddTwoNumbersM {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int add = 0, sum = 0;
+        ListNode dummy = new ListNode(0);
+        ListNode first = dummy;
+        while (l1 != null && l2 != null) {
+            sum = l1.val + l2.val + (add > 0 ? 1 : 0);
+            if (sum > 9) {
+                add = 1;
+                sum = sum % 10;
+            } else
+                add = 0;
+            ListNode a = new ListNode(sum);
+            first.next = a;
+            first = first.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        while (l1 != null) {
+
+            sum = (add > 0 ? 1 : 0) + l1.val;
+            if (sum > 9) {
+                sum = sum % 10;
+                add = 1;
+            } else
+                add = 0;
+
+            ListNode a = new ListNode(sum);
+            first.next = a;
+            l1 = l1.next;
+            first = first.next;
+        }
+        while (l2 != null) {
+            sum = (add > 0 ? 1 : 0) + l2.val;
+            if (sum > 9) {
+                sum = sum % 10;
+                add = 1;
+            } else
+                add = 0;
+
+            ListNode a = new ListNode(sum);
+            first.next = a;
+            l2 = l2.next;
+            first = first.next;
+        }
+        if (add > 0) {
+            ListNode a = new ListNode(1);
+            first.next = a;
+        }
+
+        return dummy.next;
+    }
+}
