519f0163f13a5a82173ef1942f9adeea60693f1b
Zhang Yasen <zhangyasen@didichuxing.com>
2017-06-29 22:06:53 +0800

    dfs

---
+++ src/main/java/com/valiantsen/lcpractise/string/RestoreIpAddressesM.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2017, DiDiChuXing. All rights reserved.
+ */
+package main.java.com.valiantsen.lcpractise.string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName: RestoreIpAddressesM
+ * @author zhangyasen
+ * @date 2017年6月29日
+ */
+public class RestoreIpAddressesM {
+
+    public List<String> restoreIpAddresses(String s) {
+        List<String> list = new ArrayList<>();
+        valid(s, 0, 0, "", list);
+        return list;
+    }
+
+    private void valid(String s, int index, int count, String ip, List<String> list) {
+        if (count > 4)
+            return;
+        if (count == 4 && index == s.length())
+            list.add(ip);
+        for (int i = 1; i < 4 && (index + i) <= s.length(); i++) {
+            String sub = s.substring(index, index + i);
+            if (Integer.valueOf(sub) > 255 || (sub.startsWith("0") && sub.length() > 1))
+                return;
+
+            valid(s, index + i, count + 1, ip + (count == 0 ? "" : ".") + sub, list);
+        }
+    }
+
+    public List<String> solution(String s) {
+        List<String> list = new ArrayList<>();
+        restoreIp(s, list, 0, "", 0);
+        return list;
+    }
+
+    private void restoreIp(String ip, List<String> solutions, int idx, String restored, int count) {
+        if (count > 4)
+            return;
+        if (count == 4 && idx == ip.length())
+            solutions.add(restored);
+
+        for (int i = 1; i < 4; i++) {
+            if (idx + i > ip.length())
+                break;
+            String s = ip.substring(idx, idx + i);
+            if ((s.startsWith("0") && s.length() > 1) || (i == 3 && Integer.parseInt(s) >= 256))
+                continue;
+            restoreIp(ip, solutions, idx + i, restored + s + (count == 3 ? "" : "."), count + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        RestoreIpAddressesM m = new RestoreIpAddressesM();
+        // m.solution("010010");
+        m.restoreIpAddresses("010010");
+    }
+}
