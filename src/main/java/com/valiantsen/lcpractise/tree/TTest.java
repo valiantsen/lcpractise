e0943fec4cd352ee7ec56ce86a51cf8efaac345a
Zhang Yasen <zhangyasen@didichuxing.com>
2017-09-04 18:28:26 +0800

    some questions

--- src/main/java/com/valiantsen/lcpractise/tree/TTest.java
+++ src/main/java/com/valiantsen/lcpractise/tree/TTest.java
@@ -1,10 +1,11 @@
 package main.java.com.valiantsen.lcpractise.tree;
 
+import java.math.BigInteger;
+
 public class TTest {
 
     public static void main(String[] args) {
-        String a = "af 是否";
-        String m = a.length() > 4 ? a.substring(0, 4) : a;
-        System.out.println(m);
+        BigInteger b = new BigInteger("281474976710655");
+        System.out.println(new BigInteger("564431136369211").and(b));
     }
 }
