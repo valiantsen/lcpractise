bda33953a9c9cd0c2919165a8e973dc659755249
Zhang Yasen <zhangyasen@didichuxing.com>
2017-10-15 17:05:11 +0800

    dp

---
+++ src/main/java/com/valiantsen/lcpractise/dp/CoinChangeM.java
@@ -0,0 +1,34 @@
+package main.java.com.valiantsen.lcpractise.dp;
+
+/**
+ * You are given coins of different denominations and a total amount of money amount. Write a
+ * function to compute the fewest number of coins that you need to make up that amount. If that
+ * amount of money cannot be made up by any combination of the coins, return -1.
+ * 
+ * Example 1: coins = [1, 2, 5], amount = 11 return 3 (11 = 5 + 5 + 1)
+ * 
+ * Example 2: coins = [2], amount = 3 return -1.
+ * 
+ * Note: You may assume that you have an infinite number of each kind of coin.
+ * 
+ * @ClassName: CoinChangeM
+ * @author zhangyasen
+ * @date 2017年10月15日
+ */
+public class CoinChangeM {
+
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        for (int i = 1; i < amount + 1; i++) {
+            dp[i] = amount + 1;
+        }
+        dp[0] = 0;
+        for (int i = 1; i < amount + 1; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i >= coins[j])
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
