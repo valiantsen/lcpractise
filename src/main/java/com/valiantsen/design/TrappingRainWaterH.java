ff43be23f4d31075f0d9ccf0c3a1a2053155146f
Zhang Yasen <zhangyasen@didichuxing.com>
2017-10-20 15:43:14 +0800

    trap

--- src/main/java/com/valiantsen/design/TrappingRainWaterH.java
+++ src/main/java/com/valiantsen/design/TrappingRainWaterH.java
@@ -11,5 +11,38 @@ package main.java.com.valiantsen.design;
  * @date 2017年9月21日
  */
 public class TrappingRainWaterH {
+    public int trap(int[] height) {
+        int area = 0;
+        if (height == null || height.length == 0)
+            return area;
+        int left = 0, right = height.length - 1;
+        int maxleft = height[left], maxright = height[right];
+        while (left < right) {
+            if (height[left] < height[right]) {
+                while (++left < right && height[left] < maxleft) {
+                    area += (maxleft - height[left]);
+                }
+                if (height[left] > maxleft) {
+                    maxleft = height[left];
+                }
+                continue;
+            } else {
+                while (left < --right && height[right] < maxright) {
+                    area += (maxright - height[right]);
+                }
+                if (height[right] > maxright) {
+                    maxright = height[right];
+                }
+                continue;
+            }
+        }
+        return area;
+
+    }
+
+    public static void main(String[] args) {
+        int[] inputs = {2, 1, 0, 2};
+
+    }
 
 }
